e1e6c999be933fb05e8678c284bebc0e360c396c
Muhammad Haroon <contactwithharoon@gmail.com>
2023-10-23 00:19:40 +0500

    used tailwind with react + use component with props

---
+++ Tailwind and props/tailwind_props/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './Card';
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-green-400 p-4 rounded-xl text-black mb-4'>CodexHaroon</div>
+
+      <Card name = "HP Victus" />
+      <Card />
+
+    </>
+  )
+}
+
+export default App
