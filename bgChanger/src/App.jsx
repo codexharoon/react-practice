4e4e290a744465fa60ac3a489ef414a665941427
Muhammad Haroon <contactwithharoon@gmail.com>
2023-10-27 23:26:26 +0500

    initial bg changer project

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [color, setColor] = useState('skyblue');
+
+  return (
+    <>
+      <p>Ho</p>
+    </>
+  );
+}
+
+export default App
